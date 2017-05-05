8fb3f72ef9688bca990fabd1067ca2b7f2896af4
Marko <marko.jurisic86@gmail.com>
2017-05-05 14:35:25 +0200

    Completed the Testimonials section.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      cssvars = require('postcss-simple-vars'),
+      nested = require('postcss-nested'),
+      cssImport = require('postcss-import'),
+      mixins = require('postcss-mixins');
 
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/styles.css')
